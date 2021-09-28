a82dbb2a19cb937e95bb16aa7262429535daf6f0
Tmiddleton17 <tylor.middleton@codeimmersives.com>
2021-09-28 16:36:57 -0400

    var.math

---
+++ types.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')
+
+const str = 'words and things like that';
+
+const num = 12312;
+
+console.log(str + num);
+
+// ============
+
+const x = '10';
+const y = 10;
+console.log(x + y);
+
+// ============
+
+const ourSecretVariable = 'hello';
+
+console.log(typeof ourSecretVariable);
+
+console.log(typeof input);
+
+// ============
+
+const input = prompt('Enter Anything: ');
+
+console.log('Your value is of type:', typeof input);
+
+console.log('Your value is:', input);
